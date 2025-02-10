8bdaeeabfb92ab53c7afc374f069dee212f79030
h-tiwari-kusho <150887207+h-tiwari-kusho@users.noreply.github.com>
2025-02-10 16:14:43 +0530

    Update layout.tsx added meticulous

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {(process.env.NODE_ENV === "development" || process.env.VERCEL_ENV === "preview") && (
+          // eslint-disable-next-line @next/next/no-sync-scripts
+          <script
+            data-recording-token="8qDhO6upaVctuwdPGxXnql89iUbjF6R8F79s0hhI"
+            data-is-production-environment="false"
+            src="https://snippet.meticulous.ai/v1/meticulous.js"
+          />
+        )}
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
